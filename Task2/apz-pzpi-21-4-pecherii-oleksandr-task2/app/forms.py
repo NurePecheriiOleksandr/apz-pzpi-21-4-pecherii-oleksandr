e22feab00d428ffeb592696c632f7056b8d9e3b6
NurePecheriiOleksandr <168202847+NurePecheriiOleksandr@users.noreply.github.com>
2024-05-17 23:38:57 +0300

    Update forms.py
    Added user data check while registration

--- Task2/apz-pzpi-21-4-pecherii-oleksandr-task2/app/forms.py
+++ Task2/apz-pzpi-21-4-pecherii-oleksandr-task2/app/forms.py
@@ -1,11 +1,9 @@
-"""
-Definition of forms.
-"""
-
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
 from .models import *
+import re
+
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
@@ -14,6 +12,18 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'password', 'confirm_password', 'role', 'height', 'foot_size']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+            raise forms.ValidationError("Please enter a valid email address.")
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if len(password) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters long.")
+        return password
 
     def clean_confirm_password(self):
         password = self.cleaned_data.get('password')
@@ -30,32 +40,38 @@ class RegistrationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
-    
+
+
 class LoginUserForm(AuthenticationForm):
     class Meta:
         fields = ['email', 'password']
+
 
 class OrganizerForm(forms.ModelForm):
     class Meta:
         model = Organizer
         fields = ['organizer_name', 'organizer_type']
 
+
 class GearForm(forms.ModelForm):
     class Meta:
         model = Gear
         fields = ['gear_name', 'gear_type', 'size', 'organizer']
+
 
 class DiveComputerForm(forms.ModelForm):
     class Meta:
         model = DiveComputer
         fields = ['depth', 'dive_time']
 
+
 class ActivityForm(forms.ModelForm):
     class Meta:
         model = Activity
         fields = ['activity_name', 'country', 'description', 'places', 'organizer']
 
+
 class ParticipationForm(forms.ModelForm):
     class Meta:
         model = Participation
-        fields = ['group', 'dive_computer', 'activity', 'gear']
+        fields = ['group', 'dive_computer', 'activity']
